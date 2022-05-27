46c3becc2c0567178e26f961aa13fb85c9e2ef22
Knut Rand <knutdrand@gmail.com>
2022-05-27 17:10:04 +0200

    alpha

--- pythonic_plots/__init__.py
+++ pythonic_plots/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Knut Rand"""
 __email__ = 'knutdrand@gmail.com'
 __version__ = '0.1.0'
+
+from .pythonic_plots import Module
+plt = Module()
+
+__all__ = ["plt"]
--- pythonic_plots/pythonic_plots.py
+++ pythonic_plots/pythonic_plots.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from itertools import groupby
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 @dataclass
@@ -30,7 +31,7 @@ TODO: getattr should return a callable that returns a lazyplot
 
 class Module:
 
-    def show(self, lazy_plot_struct, n_row=None, n_col=None):
+    def show(self, lazy_plot_struct, n_row=None, n_col=None, show=True, sharex=False, sharey=False):
         if len(lazy_plot_struct) == 0:
             return
         if isinstance(lazy_plot_struct[0], LazyPlot):
@@ -48,12 +49,30 @@ class Module:
             max_row = max(len(row) for row in lazy_plot_struct)
             n_row, n_col = (len(lazy_plot_struct), max_row)
 
-        fig, axes = plt.subplots(n_row, n_col)
+        fig, axes = plt.subplots(n_row, n_col, sharex=sharex, sharey=sharey)
         axes = axes.reshape(n_row, n_col)  # plt squeezes array
         for row, ax_row in zip(lazy_plot_struct, axes):
             for lazy_plot, ax in zip(row, ax_row):
                 lazy_plot(ax)
+                ax.label_outer()
+        if show:
+            plt.show()
+        return fig, axes
 
+    def save(self, lazy_plot_struct, filename):
+        self.show(lazy_plot_struct, show=False)
+        plt.savefig(filename)
+
+    def show2(self, lazy_plot_struct):
+        fig = plt.figure()
+        g = gridspec.GridSpec(len(lazy_plot_struct), 1, figure=fig)
+        for row, row_spec in zip(lazy_plot_struct, g):
+            n_col = len(row)
+            col_grid = gridspec.GridSpecFromSubplotSpec(1, n_col, subplot_spec=row_spec)
+            for lazy_plot, cell_spec in zip(row, col_grid):
+                if lazy_plot is not None:
+                    ax = fig.add_subplot(cell_spec)
+                    lazy_plot(ax)
         plt.show()
 
     def __dir__(self):
